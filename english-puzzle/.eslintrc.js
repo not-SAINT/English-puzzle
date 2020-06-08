b9877ae8a402b2b446751c4aafcc91769622b4e6
Aleksandr Sharikov <notsaint.mail@gmail.com>
2020-06-08 16:07:18 +0300

    init: start task english-puzzle

---
+++ english-puzzle/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'prettier/react',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    semi: 0,
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true,
+        singleQuote: true,
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
---
+++ english-puzzle/README.md
@@ -0,0 +1,3 @@
+# [FancyWeather](https://not-saint-english-puzzle.netlify.app/)
+
+## [Task](https://github.com/rolling-scopes-school/tasks/blob/master/tasks/rslang/english-puzzle.md)
